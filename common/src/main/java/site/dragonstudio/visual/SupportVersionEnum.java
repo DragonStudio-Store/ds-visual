04b685577a035d6302f7e0501a0330ae2793f823
aivruu <benjaalejandro206@gmail.com>
2024-05-27 18:00:05 -0400

    feat(compatibility): Create enum with versions compatible, and currently supported by the library

---
+++ common/src/main/java/site/dragonstudio/visual/SupportVersionEnum.java
@@ -0,0 +1,28 @@
+package site.dragonstudio.visual;
+
+/**
+ * This enum is used to mark all supported Minecraft versions by
+ * the library.
+ *
+ * @since 1.0.0
+ */
+public enum SupportVersionEnum {
+  /** Minecraft 1.8.8 */
+  V1_8_R3,
+  /** Minecraft 1.9.4 */
+  V1_9_R2,
+  /** Minecraft 1.10.2 */
+  V1_10_R2,
+  /** Minecraft 1.11.2 */
+  V1_11_R2,
+  /** Minecraft 1.12.2 */
+  V1_12_R2,
+  /** Minecraft 1.13.2 */
+  V1_13_R2,
+  /** Minecraft 1.14.4 */
+  V1_14_R2,
+  /** Minecraft 1.15.2 */
+  V1_15_R2,
+  /** Minecraft 1.16.5 */
+  V1_16_R3
+}
