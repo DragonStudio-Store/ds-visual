b4de607662b7e67e621bb5921dc5b0db695211f3
aivruu <benjaalejandro206@gmail.com>
2024-05-27 17:48:31 -0400

    chore: Define java-8 language level and toolchain, and implement jetbrains-annotations

--- build-logic/src/main/kotlin/lib.common-convention.gradle.kts
+++ build-logic/src/main/kotlin/lib.common-convention.gradle.kts
@@ -2,11 +2,20 @@ plugins {
     java
 }
 
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+}
+
 repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.codemc.org/nms/")
+}
+
+
+dependencies {
+  implementation("org.jetbrains:annotations:24.0.1")
 }
 
 tasks {
