57dee9137666796576319c55c3cd9fbbc8615c2f
01000010 <71404592+aivruu@users.noreply.github.com>
2024-05-27 21:03:25 -0400

    chore(version): Fix release number on enums for Minecraft versions

--- common/src/main/java/site/dragonstudio/visual/SupportVersionEnum.java
+++ common/src/main/java/site/dragonstudio/visual/SupportVersionEnum.java
@@ -12,17 +12,17 @@ public enum SupportVersionEnum {
   /** Minecraft 1.9.4 */
   V1_9_R2,
   /** Minecraft 1.10.2 */
-  V1_10_R2,
+  V1_10_R1,
   /** Minecraft 1.11.2 */
-  V1_11_R2,
+  V1_11_R1,
   /** Minecraft 1.12.2 */
-  V1_12_R2,
+  V1_12_R1,
   /** Minecraft 1.13.2 */
-  V1_13_R2,
+  V1_13_R1,
   /** Minecraft 1.14.4 */
-  V1_14_R2,
+  V1_14_R1,
   /** Minecraft 1.15.2 */
-  V1_15_R2,
+  V1_15_R1,
   /** Minecraft 1.16.5 */
   V1_16_R3
 }
