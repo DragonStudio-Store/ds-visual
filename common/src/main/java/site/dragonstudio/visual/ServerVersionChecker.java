ce8cc70950d2725cfe55af3ca695b6b86a6cade1
aivruu <benjaalejandro206@gmail.com>
2024-05-29 20:08:48 -0400

    chore(checker): Use static method for version-checking logic instead of 'static constructor' usage

--- common/src/main/java/site/dragonstudio/visual/ServerVersionChecker.java
+++ common/src/main/java/site/dragonstudio/visual/ServerVersionChecker.java
@@ -13,11 +13,18 @@ import org.bukkit.Bukkit;
 public final class ServerVersionChecker {
   /**
    * The {@link Logger} instance used to notify errors during class-loading
-   * process due to invalid server version detected. 
+   * process due to invalid server version detected.
    */
   private static final Logger LOGGER = Logger.getLogger("ServerVersionChecker");
 
-  static {
+  /**
+   * Verifies if the current on-running server version is supported
+   * by the library.
+   *
+   * @return Whether the running version is supported.
+   * @since 1.0.0
+   */
+  public static boolean verify() {
     // e.g. org.bukkit.craftbukkit.v1_8_R3
     final String packageName = Bukkit.getServer().getClass().getPackage().getName();
     // e.g. org.bukkit.craftbukkit.v1_8_R3 -> 'v1_8_R3'
@@ -27,9 +34,11 @@ public final class ServerVersionChecker {
       // validate the version, if the operation thrown an exception, indicate
       // that the current server-version isn't supported.
       SupportVersionEnum.valueOf(minecraftRelease.toUpperCase(Locale.ROOT));
+      return true;
     } catch (final IllegalArgumentException exception) {
       LOGGER.severe("Your server has a non-supported minecraft version by the DS-Visual library!");
-      LOGGER.severe("Supported Versions -> 1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2, 1.13.2, 1.14.4, 1.15.2, 1.16.5");
+      LOGGER.severe("Supported Versions - 1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2, 1.13.2, 1.14.4, 1.15.2, 1.16.5");
+      return false;
     }
   }
 }
