aad1d36a586d853de6fb17f780c32ba9d722863a
aivruu <benjaalejandro206@gmail.com>
2024-05-29 20:10:45 -0400

    chore: Fix incorrect adapters subprojects names

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,9 @@ sequenceOf("api", "test-plugin", "common").forEach {
     project(":${rootProject.name}-$it").projectDir = file(it)
 }
 
-sequenceOf("1_8_R3", "1_9_R2", "1_10_R2",
-           "1_11_R2", "1_12_R2", "1_13_R2",
-           "1_14_R2", "1_15_R2", "1_16_R3")
+sequenceOf("1_8_R3", "1_9_R2", "1_10_R1",
+           "1_11_R1", "1_12_R1", "1_13_R2",
+           "1_14_R1", "1_15_R1", "1_16_R3")
   .forEach {
     include("${rootProject.name}-$it")
     project(":${rootProject.name}-$it").projectDir = file("version/v$it")
