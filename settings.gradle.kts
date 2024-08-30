b3588358139e00cb7475cfb4e2e4a025cc12bf3a
aivruu <benjaalejandro206@gmail.com>
2024-08-30 15:46:33 -0400

    chore(settings): Modify projects' inclusion settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,18 +2,12 @@
 
 rootProject.name = "ds-visual"
 
-sequenceOf("api", "test-plugin", "common", 
-           "v1_8_R3", "v1_9_R2", "v1_10_R1",
-           "v1_11_R1", "v1_12_R1", "v1_13_R2",
-           "v1_14_R1", "v1_15_R1", "v1_16_R3",
-		   "v1_17_R1", "v1_18_R2", "v1_19_R3",
-		   "v1_20_R1", "v1_20_R2", "v1_20_R3").forEach {
-    val kerbalProject = ":${rootProject.name}-$it"
-    include(kerbalProject)
-	// Define subproject-dir for version-adapters
+sequenceOf("api", "test-plugin", "v1_8_R3", "v1_18_R2", "v1_19_R3", "v1_20_R3", "v1_21_R1").forEach {
+  val kerbalProject = ":${rootProject.name}-$it"
+  include(kerbalProject)
 	if (it.startsWith("v")) {
-        project(kerbalProject).projectDir = file("version/$it")
+    project(kerbalProject).projectDir = file("implementation/$it")
 	} else {
-	    project(kerbalProject).projectDir = file(it)
-    }
+    project(kerbalProject).projectDir = file(it)
+  }
 }
