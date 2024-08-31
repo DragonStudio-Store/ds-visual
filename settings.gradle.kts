d5bbf4d9bcc567586bda6c020a071f0bea165346
aivruu <benjaalejandro206@gmail.com>
2024-08-30 20:36:31 -0400

    chore(settings): Remove unexisting subprojects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@
 
 rootProject.name = "ds-visual"
 
-sequenceOf("api", "test-plugin", "v1_8_R3", "v1_18_R2", "v1_19_R3", "v1_20_R3", "v1_21_R1").forEach {
+sequenceOf("api", "test-plugin", "v1_8_R3", "v1_19_R3", "v1_20_R3", "v1_21_R1").forEach {
   val kerbalProject = ":${rootProject.name}-$it"
   include(kerbalProject)
 	if (it.startsWith("v")) {
